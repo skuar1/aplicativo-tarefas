05f7cff0b1da8e87ddc74a1c247f6c163a5c429d
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-08 16:05:29 -0300

    Add: Componente TaskInput

---
+++ src/components/TaskInput/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styles from './TaskInput.module.css';
+
+export default function TaskInput({ addTask }){
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value.trim()) return;
+        addTask(value);
+        setValue('');
+    };
+
+    return (
+        <form action="" className={styles.form} onSubmit={handleSubmit}>
+            <input
+             type="text" 
+             value={value}
+             onChange={e => setValue(e.target.value)}
+             placeholder="Digite sua tarefa..."
+             className={styles.input} 
+             />
+
+             <button type="submit" className={styles.button}>Adicionar</button>
+        </form>
+    )
+};
