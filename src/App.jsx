986cb717cd51741462f11c63d8d9c02240cee6aa
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-08 15:45:17 -0300

    Add: Importação dos componentes em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Clock from './components/Clock'
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
 import styles from './App.module.css';
 
 function App() {
